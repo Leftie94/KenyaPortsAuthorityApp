2080cb34693116154f3c05bc5fb445129cbd3c32
Leftie94 <morandebryan@gmail.com>
2018-11-26 15:56:24 +0300

    I have added additional java classes and resource file layouts

---
+++ app/src/main/java/com/example/leftie/Essapp/inquirynotficationmsg.java
@@ -0,0 +1,13 @@
+package com.example.leftie.Essapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inquirynotficationmsg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inquirynotficationmsg);
+    }
+}
