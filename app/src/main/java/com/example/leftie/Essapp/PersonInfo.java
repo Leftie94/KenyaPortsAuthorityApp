32ed72eec09ad98050480b338a68ce1bc034783d
Daenjel <daenjelkirigo@gmail.com>
2018-12-02 16:31:57 +0300

    creating user info model

---
+++ app/src/main/java/com/example/leftie/Essapp/PersonInfo.java
@@ -0,0 +1,92 @@
+package com.example.leftie.Essapp;
+
+public class PersonInfo {
+
+    private String id,  mFirst_name,  mSecond_name,  mDob,  mGender, mTel,  mMail,  mCity,  mEmail;
+
+    public PersonInfo(String id, String first_name, String second_name, String dob,
+                      String gender, String tel, String mail, String city, String email) {
+
+        this.id = id;
+        this.mFirst_name = first_name;
+        this.mSecond_name = second_name;
+        this.mDob = dob;
+        this.mGender = gender;
+        this.mTel = tel;
+        this.mMail = mail;
+        this.mCity = city;
+        this.mEmail = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return mFirst_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.mFirst_name = first_name;
+    }
+
+    public String getSecond_name() {
+        return mSecond_name;
+    }
+
+    public void setSecond_name(String second_name) {
+        this.mSecond_name = second_name;
+    }
+
+    public String getDob() {
+        return mDob;
+    }
+
+    public void setDob(String dob) {
+        this.mDob = dob;
+    }
+
+    public String getGender() {
+        return mGender;
+    }
+
+    public void setGender(String gender) {
+        this.mGender = gender;
+    }
+
+    public String getTel() {
+        return mTel;
+    }
+
+    public void setTel(String tel) {
+        this.mTel = tel;
+    }
+
+    public String getMail() {
+        return mMail;
+    }
+
+    public void setMail(String mail) {
+        this.mMail = mail;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        this.mCity = city;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        this.mEmail = email;
+    }
+}
